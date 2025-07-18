5210e25a801fdd9d7927d80b2a5906eb6f25f521
Hemit Patel <hemitvpatel@gmail.com>
2025-07-18 18:24:39 -0400

    added basic html form + post submission

--- src/index.php
+++ src/index.php
@@ -1,5 +1,11 @@
 <?php 
+    if ($_SERVER["REQUEST_METHOD"] == "POST) {
+        $first_name = $_POST["first_name"];
+        $last_name = $_POST["last_name"];
+        $stock_name = $_POST["stock_name"];
 
+        echo $first_name . $last_name . $stock_name;
+    }
 ?>
 
 <!DOCTYPE html>
@@ -10,6 +16,21 @@
     <title>BYCIG Stock Proposal Submission</title>
 </head>
 <body>
-    
+    <form action="<?php $_SERVER["PHP_SELF"] ?>" method="post">
+
+        <label>First Name:</label>
+        <input type="text" name="first_name">
+        <br>
+
+        <label>Last Name:</label>
+        <input type="text" name="last_name">
+        <br>
+        
+        <label>Stock:</label>
+        <input type="text" name="stock_name">
+        <br>
+        
+        <button type="submit">Submit</button>
+    </form>
 </body>
 </html>
