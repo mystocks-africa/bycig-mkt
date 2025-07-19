ed30b1a6e09cc52eae760a43e9dd4e7a24809975
Hemit Patel <hemitvpatel@gmail.com>
2025-07-19 14:01:20 -0400

    add autoload php

--- src/index.php
+++ src/index.php
@@ -1,4 +1,7 @@
 <?php 
+    // allow composer files to be used (not needed in php frameworks)
+    require 'vendor/autoload.php';
+
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $first_name = $_POST["first_name"];
         $last_name = $_POST["last_name"];
