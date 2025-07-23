2ddda2ebd3f4ea995503503722339d66235c6bcb
Hemit Patel <hemitvpatel@gmail.com>
2025-07-23 16:05:05 -0400

    update error handling more gracefully (format json encode + added an empty current_batch_number error too)

--- src/stock_cache.php
+++ src/stock_cache.php
@@ -2,11 +2,16 @@
     if ($_SERVER["REQUEST_METHOD"] == "GET") {
         header('Content-Type: application/json');
 
+        if (empty($_GET["current_batch_number"])) {
+            echo json_encode(["error" => "current_batch_number is not provider"]);
+            exit;
+        }
+
         $current_batch_number = $_GET["current_batch_number"];
         $batches_count = apcu_fetch("count");
 
         if ($current_batch_number >= $batches_count) {
-            echo json_encode("no_more_stocks");
+            echo json_encode(["error" => "No more stock cache batches available"]);
         } else {
             $batch = json_decode(apcu_fetch("symbols_" . $current_batch_number), true);            
             echo json_encode($batch);
