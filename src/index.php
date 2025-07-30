04a93f7f477c0490dfb350e742e7b1fd8373dfac
Hemit Patel <hemitvpatel@gmail.com>
2025-07-30 12:48:16 -0400

    properly sending out mysql results

--- src/index.php
+++ src/index.php
@@ -4,20 +4,35 @@ include 'utils/database.php';
 $GET_ALL_PROPOSALS = filter_input(INPUT_GET,'get_all_proposals', FILTER_SANITIZE_SPECIAL_CHARS);
 $request_method = $_SERVER['REQUEST_METHOD'];
 
-if (isset($GET_PROPOSAL_INFO) && $request_method == "GET") {
+if (isset($GET_ALL_PROPOSALS) && $request_method == "GET") {
+    header('Content-Type: application/json');
+
     $get_proposal_query = "
         SELECT email, stock_ticker, stock_name, subject_line, thesis, bid_price, target_price, proposal_file
         FROM wp_2_proposals 
     ";
 
-    $stmt = $mysqli->prepare($get_proposal_query);
-    $stmt->bind_param("s", $request_method);
-    $stmt->execute();
-    $stmt->close();
-    $result = $stmt->get_result();
+    try {
+        $stmt = $mysqli->prepare($get_proposal_query);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $stmt->close();
 
-    $result_json = json_encode($result);
-    echo $result_json;
+        $rows = [];
+        while ($row = $result->fetch_assoc()) {
+            $rows[] = $row;
+        }
+        $result_json = json_encode($rows);
+        echo $result_json;
+        exit();
+    } catch (Exception $error) {
+        $error_message = json_encode([
+            "error" => $error->getMessage()
+        ]);
+
+        echo $error_message;
+        exit();
+    }
 }
 ?>
 
--- src/static/javascript/index.js
+++ src/static/javascript/index.js
@@ -2,8 +2,8 @@ document.addEventListener("DOMContentLoaded", () => {
     fetch("index.php?get_all_proposals=true")
     .then(response => response.json())
     .then(proposals => {
-
+        console.log(proposals)
     })
 
-    
+
 })
