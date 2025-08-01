c68b40900237a56fce28b26ac4ed10315a2dd5e5
Hemit Patel <hemitvpatel@gmail.com>
2025-08-01 15:37:09 -0400

    add full name prop

--- src/json-api/auth/signup.php
+++ src/json-api/auth/signup.php
@@ -4,17 +4,23 @@ include "../../utils/database.php";
 $request_method = $_SERVER["REQUEST_METHOD"];
 
 if ($request_method == "POST") {
+    $full_name = filter_input(INPUT_POST, 'full_name', FILTER_SANITIZE_SPECIAL_CHARS);
     $email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
-    $password = filter_input(INPUT_POST, 'password', FILTER_SANITIZE_STRING);
+    $password = filter_input(INPUT_POST, 'password', FILTER_SANITIZE_SPECIAL_CHARS);
 
     $post_user_query = "
-        INSERT INTO users (email, password)
-        VALUES (?, ?)
+        INSERT INTO users (full_name, email, password)
+        VALUES (?, ?, ?)
     ";
 
     try {
         $stmt = $mysqli->prepare($post_user_query);
-        $stmt->bind_param("ss", $email, $password);
+        $stmt->bind_param(
+            "ss", 
+            $full_name,
+            $email, 
+            $password
+        );
         $stmt->execute();
         $stmt->close();
     } catch (Exception $error) {
