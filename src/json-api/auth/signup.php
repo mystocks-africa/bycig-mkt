12078af35a14bcc06d20918dfa3fb4599a84070e
Hemit Patel <hemitvpatel@gmail.com>
2025-08-01 15:33:02 -0400

    add sign up logic

--- src/json-api/auth/signup.php
+++ src/json-api/auth/signup.php
@@ -4,5 +4,25 @@ include "../../utils/database.php";
 $request_method = $_SERVER["REQUEST_METHOD"];
 
 if ($request_method == "POST") {
-    
+    $email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
+    $password = filter_input(INPUT_POST, 'password', FILTER_SANITIZE_STRING);
+
+    $post_user_query = "
+        INSERT INTO users (email, password)
+        VALUES (?, ?)
+    ";
+
+    try {
+        $stmt = $mysqli->prepare($post_user_query);
+        $stmt->bind_param("ss", $email, $password);
+        $stmt->execute();
+        $stmt->close();
+    } catch (Exception $error) {
+        $error_message = json_encode([
+            "error" => $error->getMessage()
+        ]);
+
+        echo $error_message;
+    }
+
 }
