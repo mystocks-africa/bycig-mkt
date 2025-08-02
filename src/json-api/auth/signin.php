5c0619b2ffbc32dcaca9f8279ce4401a64c0c6c1
Hemit Patel <hemitvpatel@gmail.com>
2025-08-02 12:57:01 -0400

    update assign session logic (it should be email not user id, no longer part of the table itself)

--- src/json-api/auth/signin.php
+++ src/json-api/auth/signin.php
@@ -7,12 +7,12 @@ include $BASE_DIR . "utils/memcached.php";
 $request_method = $_SERVER["REQUEST_METHOD"];
 $EXPIRATION_DAYS = 60*60*24*30; // 30 days
 
-function assign_session($role, $user_id) {
+function assign_session($role, $email) {
     global $memcached;
     global $EXPIRATION_DAYS;
     
     $session_id = bin2hex(random_bytes(32)); // 64 character hex string
-    $memcached->set($session_id, "$user_id, $role", $EXPIRATION_DAYS);
+    $memcached->set($session_id, "$email, $role", $EXPIRATION_DAYS);
     
     return $session_id;
 }
@@ -23,13 +23,13 @@ if ($request_method == "POST") {
     $hash_password = password_hash($password, PASSWORD_DEFAULT);
 
     $get_user_query = "
-        SELECT id, role
+        SELECT email, role
         FROM users 
         WHERE email = ? AND password = ?
         LIMIT 1;
     ";
 
-    $stmt = $mysqli->prepare("");
+    $stmt = $mysqli->prepare($get_user_query);
     $stmt->bind_param(
         "ss", 
         $email,
@@ -40,8 +40,8 @@ if ($request_method == "POST") {
     $user = $result->fetch_assoc();
     $stmt->close();
 
-    if (isset($user_id)) {
-        $session_id = assign_session($user['role'], $user['id']);
+    if (isset($user)) {
+        $session_id = assign_session($user['role'], $user['email']);
         setcookie('session_id', $session_id, time() + (86400 *24*60*60),'/');
         echo json_encode([
             'status' => 'success',
