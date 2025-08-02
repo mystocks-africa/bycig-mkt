f091b2d22d42de81a48a81070145d63c3388bd50
Hemit Patel <hemitvpatel@gmail.com>
2025-08-02 13:52:34 -0400

    add some spacing

--- src/utils/memcached.php
+++ src/utils/memcached.php
@@ -1,6 +1,7 @@
 <?php
 try {
     $memcached = new Memcached();
+    
     // Connect via Unix socket (local communication)
     $memcached->addServer('/tmp/memcached.sock', 0);
 } catch (Exception $error) {
