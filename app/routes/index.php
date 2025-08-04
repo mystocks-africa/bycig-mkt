40e0625cbb4e22ff4273231dea761532719d0a53
Hemit Patel <hemitvpatel@gmail.com>
2025-08-04 16:58:08 -0400

    link sign out method to router + organize stuff

--- app/routes/index.php
+++ app/routes/index.php
@@ -9,15 +9,19 @@ use App\Router;
 
 $router = new Router();
 
+// Home get methods
 $router->get('/', HomeController::class, 'index');
 $router->get('/favicon.ico', HomeController::class, 'favicon');
 $router->get('/redirect', HomeController::class, 'redirect');
 
+// Authentication get methods 
 $router->get('/signup', AuthController::class,'signUp');
 $router->get('/signin', AuthController::class, 'signIn');
 $router->get('/signout', AuthController::class,'signOut');
 
+// Authentication post methods
 $router->post('/signin', AuthController::class,'signInPost');
 $router->post("/signup", AuthController::class, 'signUpPost');
+$router->post('/signout', AuthController::class,'signOutPost');
 
 $router->dispatch();
