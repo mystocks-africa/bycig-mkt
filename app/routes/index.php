13713b489f3556bc694e22ffc77392f041c06055
Hemit Patel <hemitvpatel@gmail.com>
2025-08-05 12:23:41 -0400

    udpate import

--- app/routes/index.php
+++ app/routes/index.php
@@ -1,6 +1,6 @@
 <?php
-include_once __DIR__ . "/../Controllers/HomeController.php";
-include_once __DIR__ . "/../Controllers/AuthController.php";
+include_once __DIR__ . "/../controllers/home/Controller.php";
+include_once __DIR__ . "/../controllers/auth/Controller.php";
 include_once __DIR__ . "/Router.php";
 
 use App\Controllers\AuthController;
@@ -15,13 +15,13 @@ $router->get('/favicon.ico', HomeController::class, 'favicon');
 $router->get('/redirect', HomeController::class, 'redirect');
 
 // Authentication get methods 
-$router->get('/signup', AuthController::class,'signUp');
-$router->get('/signin', AuthController::class, 'signIn');
-$router->get('/signout', AuthController::class,'signOut');
+$router->get('/auth/signup', AuthController::class,'signUp');
+$router->get('/auth/signin', AuthController::class, 'signIn');
+$router->get('/auth/signout', AuthController::class,'signOut');
 
 // Authentication post methods
-$router->post('/signin', AuthController::class,'signInPost');
-$router->post("/signup", AuthController::class, 'signUpPost');
-$router->post('/signout', AuthController::class,'signOutPost');
+$router->post('/auth/signin', AuthController::class,'signInPost');
+$router->post("/auth/signup", AuthController::class, 'signUpPost');
+$router->post('/auth/signout', AuthController::class,'signOutPost');
 
 $router->dispatch();
