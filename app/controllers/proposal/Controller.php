74225fa2860dcf85f4b78ef8de3b8d201fa094e5
Hemit Patel <hemitvpatel@gmail.com>
2025-08-06 14:54:46 -0400

    add auth checker

--- app/controllers/proposal/Controller.php
+++ app/controllers/proposal/Controller.php
@@ -33,6 +33,8 @@ class ProposalController extends Controller {
 
     public function submit() 
     {
+        parent::redirectIfNotAuth();
+
         $clusterLeadersAssoc = User::findAllClusterLeaders();
 
         // O(n) is fine here because cluster leaders length will always be small
