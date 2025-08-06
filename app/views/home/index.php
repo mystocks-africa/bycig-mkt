6d9334b8104e16ce6448e0a361c345d574fd80d7
Hemit Patel <hemitvpatel@gmail.com>
2025-08-05 20:15:46 -0400

    add js and greeting based on time

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -4,18 +4,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Proposal Submissions</title>
+    <script src="/static/javascript/index.js"></script>
     <link rel="stylesheet" href="/static/css/index.css">
-    <script>
-        function goToSubmitProposal() {
-            window.location.href = '/proposals/submit';
-        }
-    </script>
 </head>
 <body>
     <button id="plus-btn" onclick="goToSubmitProposal()">+</button>
 
     <div class="demo-section">
-      <h1>Welcome to BYCIG</h1>
+      <h1 id="greeting"></h1>
       <p>Start by submitting a new proposal or exploring existing ones.</p>
     </div>
 
