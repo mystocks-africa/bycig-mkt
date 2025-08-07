fc4637536fdb5307a0eedaa92f4855738a19d5e3
Hemit Patel <hemitvpatel@gmail.com>
2025-08-07 13:15:19 -0400

    add a fail safe method of posting to ftp

--- app/controllers/proposal/Controller.php
+++ app/controllers/proposal/Controller.php
@@ -7,6 +7,7 @@ include_once __DIR__ . "/../../../utils/env.php";
 
 use App\Controller;
 use App\Models\Proposal;
+use Exception;
 
 class ProposalController extends Controller {
 
@@ -20,7 +21,7 @@ class ProposalController extends Controller {
 
     private function uploadToFTP($file) 
     {
-        $ftp_conn = ftp_connect($this->env["FTP_SERVER"]) or die("FTP error");
+        $ftp_conn = ftp_connect($this->env["FTP_SERVER"]) or throw new Exception("Failed to connect");
         ftp_login($ftp_conn, $this->env["FTP_USER"], $this->env["FTP_PASS"]);
 
         $filename = "uploads/" . bin2hex(random_bytes(5)) . ".pdf";
@@ -28,6 +29,24 @@ class ProposalController extends Controller {
         ftp_close($ftp_conn);
         return $result ? $filename : false;
     }
+
+    private function deleteFromFTP($fileName)
+    {
+        $ftp_conn = ftp_connect($this->env["FTP_SERVER"]) or throw new Exception("Failed to connect");
+        
+        if (!ftp_login($ftp_conn, $this->env["FTP_USER"], $this->env["FTP_PASS"])) {
+            ftp_close($ftp_conn);
+            throw new Exception("FTP login failed");
+        }
+
+        if (!ftp_delete($ftp_conn, $fileName)) {
+            ftp_close($ftp_conn);
+            throw new Exception("Failed to delete file: $fileName");
+        }
+
+        ftp_close($ftp_conn);
+    }
+
 
     public function submit() 
     {
@@ -48,32 +67,39 @@ class ProposalController extends Controller {
     
     public function submitPost() 
     {
-        $session = parent::redirectIfNotAuth(true);
-        
+        try {
+            $session = parent::redirectIfNotAuth(true);
+            
 
-        $stockTicker = filter_input(INPUT_POST, 'stock_ticker', FILTER_SANITIZE_SPECIAL_CHARS);
-        $stockName = filter_input(INPUT_POST, 'stock_name', FILTER_SANITIZE_SPECIAL_CHARS);
-        $subjectLine = filter_input(INPUT_POST, 'subject_line', FILTER_SANITIZE_SPECIAL_CHARS);
-        $thesis = filter_input(INPUT_POST, 'thesis', FILTER_SANITIZE_SPECIAL_CHARS);
-        $bidPrice = filter_input(INPUT_POST, 'bid_price', FILTER_VALIDATE_FLOAT);
-        $targetPrice = filter_input(INPUT_POST, 'target_price', FILTER_VALIDATE_FLOAT);
-        $proposalFile = $_FILES["proposal_file"] ?? null;
+            $stockTicker = filter_input(INPUT_POST, 'stock_ticker', FILTER_SANITIZE_SPECIAL_CHARS);
+            $stockName = filter_input(INPUT_POST, 'stock_name', FILTER_SANITIZE_SPECIAL_CHARS);
+            $subjectLine = filter_input(INPUT_POST, 'subject_line', FILTER_SANITIZE_SPECIAL_CHARS);
+            $thesis = filter_input(INPUT_POST, 'thesis', FILTER_SANITIZE_SPECIAL_CHARS);
+            $bidPrice = filter_input(INPUT_POST, 'bid_price', FILTER_VALIDATE_FLOAT);
+            $targetPrice = filter_input(INPUT_POST, 'target_price', FILTER_VALIDATE_FLOAT);
+            $proposalFile = $_FILES["proposal_file"] ?? null;
 
-        $fileName = $this->uploadToFTP($proposalFile);
+            $fileName = $this->uploadToFTP($proposalFile);
 
-        if (!$stockTicker || !$stockName || !$subjectLine || !$thesis || !$bidPrice || !$targetPrice || !$fileName ) {
-            parent::redirectToResult("Error in form input", "error");
-            exit();
+            if (!$stockTicker || !$stockName || !$subjectLine || !$thesis || !$bidPrice || !$targetPrice || !$fileName ) {
+                parent::redirectToResult("Error in form input", "error");
+                exit();
+            }
+
+            $proposal = new Proposal($session["email"], $stockTicker, $stockName, $subjectLine, $thesis, $bidPrice, $targetPrice, $fileName);
+            $proposal->createProposal();
+
+            if ($proposal && $proposal instanceof Exception) {
+                parent::redirectToResult("Error in submitting proposal", "error");
+                exit();
+            }
+
+            parent::redirectToResult("Success in submitting proposal", "success");
+        } catch (Exception $error) {
+            // Delete the file if there was an error to avoid orphaned files
+            $this->deleteFromFTP($fileName);
+            
+            parent::redirectToResult($error->getMessage(),"error");
         }
-
-        $proposal = new Proposal($session["email"], $stockTicker, $stockName, $subjectLine, $thesis, $bidPrice, $targetPrice, $fileName);
-        $proposal->createProposal();
-
-        if ($proposal && $proposal instanceof Exception) {
-            parent::redirectToResult("Error in submitting proposal", "error");
-            exit();
-        }
-
-        parent::redirectToResult("Success in submitting proposal", "success");
     }
 }
