e01faad6dcfcf58dc181d0dd14d4e8e26bc12bd9
Hemit Patel <hemitvpatel@gmail.com>
2025-08-08 14:16:15 -0400

    rename

--- app/controllers/admin/Controller.php
+++ app/controllers/admin/Controller.php
@@ -6,8 +6,8 @@ include_once __DIR__ . "/../../models/proposals/Model.php";
 include_once __DIR__ . "/../../models/holdings/Model.php";
 
 use App\Controller;
-use App\Models\Proposal;
-use App\Models\Holding;
+use App\Models\ProposalModel;
+use App\Models\HoldingModel;
 use Exception;
 
 class AdminController extends Controller 
@@ -16,12 +16,11 @@ class AdminController extends Controller
     {
         $session = parent::redirectIfNotClusterLeader();
 
-        $proposals = Proposal::findProposalByClusterLeader($session['email']);
+        $proposals = ProposalModel::findProposalByClusterLeader($session['email']);
         parent::render("admin/index", [
             'proposals' => $proposals
         ]);
     }
-
 
     // Sending JSON in these 2 routes because fetch api in js is dealing w/ them
     public function handleProposalStatusPost() 
@@ -35,14 +34,14 @@ class AdminController extends Controller
                 throw new Exception('Status is not properly formatted');
             }
 
-            Proposal::updateProposalStatus($postId, $clusterLeaderEmail, $status);
+            ProposalModel::updateProposalStatus($postId, $clusterLeaderEmail, $status);
 
             // Keep proposal even when a new holding w/ proposal info is created
             // Cluster leader has to delete it themselves in admin portal 
 
             if ($status == 'accept') {
-                $proposal = Proposal::findProposalById($postId);
-                $holding = new Holding(
+                $proposal = ProposalModel::findProposalById($postId);
+                $holding = new HoldingModel(
                     $proposal['email'],
                     $proposal['stock_ticker'],
                     $proposal['stock_name'],
@@ -61,31 +60,6 @@ class AdminController extends Controller
                 'status'=> 'error',
                 'error'=> $error->getMessage(),
             ]);
-        }
-    }
-
-    public function deleteProposal()
-    {
-        $session = parent::redirectIfNotClusterLeader();
-        try {
-            $postId = filter_input(INPUT_GET, 'post_id', FILTER_SANITIZE_NUMBER_INT);
-            $proposal = Proposal::findProposalById($postId);
-            
-            if (empty($proposal)) {
-                throw new Exception("Proposal is not found. You are deleting something that doesn't exist");
-            }
-
-            Proposal::deleteProposal($postId, $session['email']);
-            parent::redirectToResult('Deleted proposal successfully', 'success');
-
-            echo json_encode([
-                'status'=> 'success'
-            ]);
-        } catch(Exception $error) {
-            echo json_encode([
-                'status'=> 'error',
-                'error'=> $error->getMessage(),
-            ]);        
         }
     }
 }
