00f5a3816436964373fcde52a3473f59020a6494
Hemit Patel <hemitvpatel@gmail.com>
2025-08-10 00:39:36 -0400

    move functionality

--- app/core/Memcachedh.php
+++ app/core/Memcachedh.php
@@ -17,48 +17,19 @@ class Memcachedh {
         $this->memcached = $memcached;
     }
 
-    public function setSession(string $email, string $role) 
-    {        
-        $EXPIRATION_DAYS = 60*60*24*30; // 30 days in seconds
-        $sessionId = bin2hex(random_bytes(32)); // 64 character hex string
-        $this->memcached->set($sessionId, "$email, $role", $EXPIRATION_DAYS);
-        
-        return $sessionId;
+    public function setKeyValue($key, $value, $expiration)
+    {
+        if ($expiration) $this->memcached->set($key, $value, $expiration);
+        else $this->memcached->set($key, $value);
     }
 
-    public function getSession() 
+    public function deleteKeyValue($key)
     {
-        // Check if session_id cookie is set and not empty
-        if (empty($_COOKIE["session_id"])) {
-            return false;
-        }
-
-        $session_id_cookie = $_COOKIE["session_id"];
-        $session = $this->memcached->get($session_id_cookie);
-
-        if (!$session) {
-            // Clear the session cookie so that the navbar UI is updated
-            if (isset($_COOKIE["session_id"])) {
-                $auth = new AuthController();
-                $auth->clearSessionCookie();
-            }
-
-            return false;
-        }  
-
-        $parts = explode(",", $session);
-
-        $sessionAssoc = [
-            "email" => trim($parts[0]),
-            "role" => trim($parts[1])
-        ];
-
-        return $sessionAssoc;    
+        $this->memcached->delete($key);
     }
 
-    public function clearSession()
+    public function getKeyValue($key)
     {
-        $session_id = $_COOKIE['session_id'];
-        $this->memcached->delete($session_id);
+        return $this->memcached->get($key)
     }
 }
