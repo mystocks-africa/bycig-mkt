d5237622dbea2ec267aed22e3722b89fb6d244ae
Hemit Patel <hemitvpatel@gmail.com>
2025-08-12 11:28:48 -0400

    added proper method signatures

--- app/core/interfaces/IModel.php
+++ app/core/interfaces/IModel.php
@@ -1,6 +1,10 @@
-<?php 
+<?php  
 
 interface IModel
 {
-  
+    public function create(int $id): void;
+    public function update(int $id): void;
+    public function delete(int $id): void;
+    public function getAll(): array;
+    public function get(int $id): array;
 }
