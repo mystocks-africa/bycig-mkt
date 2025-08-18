d28edd4f98f6920162cb95f67f0765bf254e7e75
Hemit Patel <hemitvpatel@gmail.com>
2025-08-18 14:39:58 -0400

    add generateCode method

--- app/core/VerificationCode.php
+++ app/core/VerificationCode.php
@@ -1,6 +1,23 @@
 <?php
 
-class VerificationCode 
+namespace App\Core;
+include_once __DIR__ . "/../core/templates/MemcachedTemplate.php";
+include_once __DIR__ . "/Session.php";
+
+use App\Core\Templates\MemcachedTemplate;
+use App\Core\Session;
+
+class VerificationCode extends MemcachedTemplate
 {
-    
+    public static function generateCode() {
+        $memcached = parent::getMemcached();
+
+        $email = Session::getSession()["email"];
+        $code = rand(10000,99999);
+
+        $expiration = 5 * 60; // 5 minutes in seconds
+        $memcached->set($email, $code, $expiration);
+
+        parent::removeMemcached($memcached);
+    }
 }
