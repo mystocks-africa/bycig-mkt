c14a8a86362360126b42ba193194f4acaa56ff3f
Hemit Patel <hemitvpatel@gmail.com>
2025-08-21 10:21:54 -0400

    added user repositry to profile controller so that we get the user info to populate form

--- app/controllers/profile/Controller.php
+++ app/controllers/profile/Controller.php
@@ -2,15 +2,27 @@
 namespace App\Controllers;
 
 include_once __DIR__ . "/../../core/Controller.php";
+include_once __DIR__ . "/../../models/user/Repository.php";
 
 use App\Core\Controller;
-
+use App\Models\Repository\UserRepository;
 
 class ProfileController 
 {
+    private UserRepository $userRepository;
+
+    public function __construct() {
+        $this->userRepository = new UserRepository();
+    }
+
     public function index() 
     {
-        Controller::redirectIfNotAuth();
-        Controller::render("profile/index");
+        $session = Controller::redirectIfNotAuth(returnSession:true);
+
+        $user = $this->userRepository->findByEmail($session["email"]);
+
+        Controller::render("profile/index", [
+            "user"=>$user
+        ]);
     }
 }
