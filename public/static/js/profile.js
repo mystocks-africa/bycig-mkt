fcc5191e5a8f1c0e30a8f404ab2cc3a95662684a
Hemit Patel <hemitvpatel@gmail.com>
2025-08-21 10:13:39 -0400

    remove comments

--- public/static/js/profile.js
+++ public/static/js/profile.js
@@ -8,20 +8,16 @@ function handleToggleScreen(screen) {
     const holdingsTabElement = document.getElementById('holdings-tab');
 
     if (screen === 'info') {
-        // Show info screen, hide holdings screen
         userInfoElement.style.display = 'block';
         userHoldingsElement.style.display = 'none';
 
-        // Set active tab correctly
         infoTabElement.classList.add('active');
         holdingsTabElement.classList.remove('active');
     } 
     else if (screen === 'holdings') {
-        // Show holdings screen, hide info screen
         userHoldingsElement.style.display = 'block';
         userInfoElement.style.display = 'none';
 
-        // Set active tab correctly
         holdingsTabElement.classList.add('active');
         infoTabElement.classList.remove('active');
     } 
