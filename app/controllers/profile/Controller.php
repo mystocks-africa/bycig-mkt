44224c9b305b8253bcf38268d57b24780c8893cd
Hemit Patel <hemitvpatel@gmail.com>
2025-08-21 09:43:40 -0400

    add index function

--- app/controllers/profile/Controller.php
+++ app/controllers/profile/Controller.php
@@ -1,6 +1,16 @@
 <?php
+namespace App\Controllers;
+
+include_once __DIR__ . "/../../core/Controller.php";
+
+use App\Core\Controller;
+
 
 class ProfileController 
 {
-    
+    public function index() 
+    {
+        Controller::redirectIfNotAuth();
+        Controller::render("profile/index");
+    }
 }
