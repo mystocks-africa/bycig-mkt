4140d05cf1ab3f8a4e3ad0732584989b3e8b26ea
Hemit Patel <hemitvpatel@gmail.com>
2025-08-22 15:41:31 -0400

    added holdings ui

--- app/views/profile/index.php
+++ app/views/profile/index.php
@@ -54,7 +54,33 @@
 
 
     <div id="user-holdings">
-        Holding Section
+        <div id="grid-container">
+        <?php if (!empty($holdings)): ?>
+                <?php foreach ($holdings as $holding): ?>
+                    <?php
+                        $id = $holding["id"];
+                        $investor = $holding["investor"];
+                        $stockTicker = $holding["stock_ticker"];
+                        $stockName = $holding["stock_name"];
+                        $proposalFile = $holding["proposal_file"];
+                    ?>
+                    <div key=<?= $id ?> class="card" style="text-decoration: none; color: black;">
+                        <h3 class="truncate"><?= $stockName ?></h3>
+                        <p><strong>Stock Ticker:</strong> <?= $stockTicker ?></p>
+                        <p><strong>Investor:</strong> <?= $investor ?></p>
+                        <p><strong>File:</strong> <a class="underline-text" href="/uploads/<?= $proposalFile ?>"">click here</a></p>
+
+                        <div class="delete-btn-wrapper">
+                            <button class="delete-btn">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-delete-icon lucide-delete"><path d="M10 5a2 2 0 0 0-1.344.519l-6.328 5.74a1 1 0 0 0 0 1.481l6.328 5.741A2 2 0 0 0 10 19h10a2 2 0 0 0 2-2V7a2 2 0 0 0-2-2z"/><path d="m12 9 6 6"/><path d="m18 9-6 6"/></svg>
+                            </button>
+                        </div>
+                    </div>
+                <?php endforeach; ?>
+            <?php else: ?>
+                <p>No holdings found.</p>
+            <?php endif; ?>
+        </div>   
     </div>
 </body>
 </html>
