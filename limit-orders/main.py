3ad7ccd10c0561da99a1dd61310d3a7a501ecfee
Hemit Patel <hemitvpatel@gmail.com>
2025-08-23 14:47:43 -0400

    added supported stocks and a function for placing limit order

--- limit-orders/main.py
+++ limit-orders/main.py
@@ -1,1 +1,7 @@
-print("Hello world!");
+supported_stocks = ["AAPL", "MSFT", "GOOGL", "AMZN", "TSLA"]
+
+def place_limit_order(): 
+    for stock in supported_stocks:
+        print(f"Placing limit order for {stock}")
+
+place_limit_order()
