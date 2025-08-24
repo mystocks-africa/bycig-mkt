67d076096d9c6537de084bc9be8e55a21d3a2fa4
Hemit Patel <hemitvpatel@gmail.com>
2025-08-24 16:55:25 -0400

    update so its a recurvsive function

--- limit-orders/classes/Response.py
+++ limit-orders/classes/Response.py
@@ -4,8 +4,8 @@ class Response:
         if isinstance(obj, bytes):
             return obj.decode()
         elif isinstance(obj, dict):
-            return {k: decode_bytes(v) for k, v in obj.items()}
+            return {k: Response.decode_bytes(v) for k, v in obj.items()}
         elif isinstance(obj, list):
-            return [decode_bytes(x) for x in obj]
+            return [Response.decode_bytes(x) for x in obj]
         else:
             return obj
