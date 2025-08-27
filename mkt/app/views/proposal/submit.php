aa3b0cded6c5d0e096863febea67115b93a06ce3
Hemit Patel <hemitvpatel@gmail.com>
2025-08-27 18:16:22 -0400

    bring br tags closer in code (no real changes)

--- mkt/app/views/proposal/submit.php
+++ mkt/app/views/proposal/submit.php
@@ -19,32 +19,26 @@
 
         <label for="stock_name">Stock Name:</label><br>
         <input type="text" id="stock_name" name="stock_name" maxlength="255" required>
-
         <br><br>
 
         <label for="subject_line">Subject Line:</label><br>
         <input type="text" id="subject_line" name="subject_line" maxlength="255" required>
-
         <br><br>
 
         <label for="thesis">1 Sentence Thesis:</label><br>
         <textarea id="thesis" name="thesis" maxlength="1000" required></textarea>
-
         <br><br>
 
         <label for="bid_price">Bid Price (where you want us to buy at):</label><br>
         <input type="number" id="bid_price" name="bid_price" step="0.01" min="0" required>
-
         <br><br>
 
         <label for="target_price">Target Price (must be ≥ Bid Price):</label><br>
         <input type="number" id="target_price" name="target_price" step="0.01" min="0" required>
-
         <br><br>
 
         <label for="proposal_file">Upload Proposal (PDF only, max 5MB):</label><br>
         <input type="file" id="proposal_file" name="proposal_file" accept="application/pdf" required>
-
         <br><br>
 
         <button type="submit">Submit Proposal</button>
