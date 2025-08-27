580e8fe154d8cd74d72848c433d6267600ed0787
Hemit Patel <hemitvpatel@gmail.com>
2025-08-27 10:25:11 -0400

    added responsivness to navbar

--- mkt/app/views/navbar.php
+++ mkt/app/views/navbar.php
@@ -1,34 +1,29 @@
-<?php
-ob_start();
-$loggedIn = isset($_COOKIE["session_id"]);
-?>
 <!DOCTYPE html>
 <html>
 <head>
     <link rel="stylesheet" href="/static/css/navbar.css">
 </head>
 <body>
+    <!-- MOBILE SCREEN -->
     <nav>
         <a href="/" class="logo">BYCIG MKT</a>
-        <button class="nav-toggle" aria-label="Toggle navigation">
-            <span class="hamburger"></span>
-        </button>
+        <div class="nav-toggle" aria-label="Toggle navigation">
+            <div class="bar"></div>
+            <div class="bar"></div>
+            <div class="bar"></div>
+        </div>
         <ul class="nav-links">
             <li><a href="/auth/forgot-pwd">Forgot password?</a></li>
-            <?php if ($loggedIn): ?>
-                <li><a href="/proposals/submit">Create Proposal</a></li>
-                <li><a href="/profile">User Profile</a></li>
-                <li>
-                    <form action="/auth/signout" method="POST">
-                        <button id="signout-btn" type="submit">Sign out</button>
-                    </form>  
-                </li>  
-            <?php else: ?>
-                <li><a href="/auth/signin">Sign in</a></li>
-                <li><a href="/auth/signup">Sign up</a></li>
-            <?php endif; ?>
+            <li><a href="/proposals/submit">Create Proposal</a></li>
+            <li><a href="/profile">User Profile</a></li>
+            <li>
+                <form action="/auth/signout" method="POST">
+                    <button id="signout-btn" type="submit">Sign out</button>
+                </form>  
+            </li>  
         </ul>
     </nav>
-    <div class="nav-overlay"></div>
+
+    <script src="/static/js/navbar.js"></script>
 </body>
 </html>
