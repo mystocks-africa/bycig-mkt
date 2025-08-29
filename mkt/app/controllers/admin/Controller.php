ac50a830ebd034f7341988b210e20c0e8dbad264
Hemit Patel <hemitvpatel@gmail.com>
2025-08-29 15:26:51 -0400

    added service layer

--- mkt/app/controllers/admin/Controller.php
+++ mkt/app/controllers/admin/Controller.php
@@ -3,44 +3,33 @@
 namespace App\Controllers;
 
 include_once __DIR__ . "/../../core/controller/Controller.php";
-include_once __DIR__ . "/../../core/templates/DbTemplate.php";
 include_once __DIR__ . "/../../core/auth/Guard.php";
 
-include_once __DIR__ . "/../../models/proposals/Repository.php";
-include_once __DIR__ . "/../../models/holdings/Repository.php";
-include_once __DIR__ . "/../../models/holdings/Entity.php";
+include_once __DIR__ . "/../../services/admin/Service .php";
 
 use App\Core\Controller;
 use App\Core\Session;
 use App\Core\Auth\AuthGuard;
 
-use App\DbTemplate;
-use App\Models\Repository\HoldingRepository;
-use App\Models\Entity\HoldingEntity;
-use App\Models\Repository\ProposalRepository;
+use App\Services\Admin\AdminService;
 use Exception;
 
 class AdminController 
 {
-    private HoldingRepository $holdingRepository;
-    private ProposalRepository $proposalRepository;
-    private DbTemplate $db;
     private Session $session;
     private AuthGuard $authGuard;
+    private AdminService $adminService;
 
     public function __construct() {
-        $this->db = new DbTemplate();
-        $this->holdingRepository = new HoldingRepository($this->db->getPdo());
-        $this->proposalRepository = new ProposalRepository($this->db->getPdo());
         $this->session = new Session();
         $this->authGuard = new AuthGuard($this->session);
+        $this->adminService = new AdminService();
     }
 
     public function index()
     {
         $this->authGuard->redirectIfNotAuth();
-
-        $proposals = $this->proposalRepository->findByClusterLeader($this->session->getSession()['email']);
+        $proposals = $this->adminService->getProposalsByClusterLeader($this->session->getSession()['email']);
         Controller::render("admin/index", [
             'proposals' => $proposals
         ]);
@@ -55,26 +44,7 @@ class AdminController
             $clusterLeaderEmail = filter_input(INPUT_GET, 'cluster_leader_email', FILTER_SANITIZE_SPECIAL_CHARS);
             $status = filter_input(INPUT_GET, 'status', FILTER_SANITIZE_SPECIAL_CHARS);
 
-            if ($status != 'accept' && $status != 'decline') {
-                throw new Exception('Status is not properly formatted');
-            }
-
-            $this->proposalRepository->updateStatus($postId, $clusterLeaderEmail, $status);
-
-
-            if ($status == 'accept') {
-                $proposal = $this->proposalRepository->findById($postId);
-                $holdingEntity = new HoldingEntity(
-                    $proposal['email'],
-                    $proposal['stock_ticker'],
-                    $proposal['stock_name'],
-                    $proposal['bid_price'],
-                    $proposal['target_price'],
-                    $proposal['proposal_file']
-                );
-
-                $this->holdingRepository->save($holdingEntity);
-            }
+            $this->adminService->processProposalDecision($postId, $clusterLeaderEmail, $status);
 
             Controller::redirectToResult('Posted proposal successfully', 'success');
         } catch (Exception $error) {
@@ -88,13 +58,7 @@ class AdminController
 
         try {
             $postId = filter_input(INPUT_GET, 'post_id', FILTER_SANITIZE_NUMBER_INT);
-            $proposal = $this->proposalRepository->findById($postId);
-            
-            if (empty($proposal)) {
-                throw new Exception("Proposal is not found. You are deleting something that doesn't exist");
-            }
-
-            $this->proposalRepository->delete($postId, $this->session->getSession()['email']);
+            $this->adminService->deleteProposalById($postId, $this->session->getSession()['email']);
             Controller::redirectToResult('Deleted proposal successfully', 'success');
         } catch(Exception $error) {
             Controller::redirectToResult('Error in deleting proposal', 'error');       
---
+++ mkt/app/services/admin/Service.php
@@ -0,0 +1,68 @@
+<?php
+namespace App\Services\Admin;
+
+include_once __DIR__ . "/../../core/templates/DbTemplate.php";
+
+include_once __DIR__ . "/../../models/proposals/Repository.php";
+include_once __DIR__ . "/../../models/holdings/Repository.php";
+include_once __DIR__ . "/../../models/holdings/Entity.php";
+
+use App\DbTemplate;
+use App\Models\Repository\HoldingRepository;
+use App\Models\Entity\HoldingEntity;
+use App\Models\Repository\ProposalRepository;
+use Exception;
+
+class AdminService
+{
+    private DbTemplate $db;
+    private ProposalRepository $proposalRepository;
+    private HoldingRepository $holdingRepository;
+
+
+    public function __construct()
+    {
+        $this->db = new DbTemplate();
+        $this->proposalRepository = new ProposalRepository($this->db->getPdo());
+    }
+
+    public function getProposalsByClusterLeader(string $email): array
+    {
+        return $this->proposalRepository->findByClusterLeader($email);
+    }
+
+    public function processProposalDecision(string $postId, string $clusterLeaderEmail, string $status): void 
+    {
+        if ($status != 'accept' && $status != 'decline') {
+                throw new Exception('Status is not properly formatted');
+        }
+
+        $this->proposalRepository->updateStatus($postId, $clusterLeaderEmail, $status);
+
+
+        if ($status == 'accept') {
+            $proposal = $this->proposalRepository->findById($postId);
+            $holdingEntity = new HoldingEntity(
+                $proposal['email'],
+                $proposal['stock_ticker'],
+                $proposal['stock_name'],
+                $proposal['bid_price'],
+                $proposal['target_price'],
+                $proposal['proposal_file']
+            );
+
+            $this->holdingRepository->save($holdingEntity);
+        }
+    }
+
+    public function deleteProposalById(int $postId, string $email): void
+    {
+        $proposal = $this->proposalRepository->findById($postId);
+            
+        if (empty($proposal)) {
+            throw new Exception("Proposal is not found. You are deleting something that doesn't exist");
+        }
+
+        $this->proposalRepository->delete($postId, $email);
+    }
+}
