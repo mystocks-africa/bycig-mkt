a7eb0ee007999dbdbb10db39dc54bd547dfbf90b
Hemit Patel <hemitvpatel@gmail.com>
2025-08-29 12:31:45 -0400

    migrated home controller to auth guard core service

--- mkt/app/controllers/home/Controller.php
+++ mkt/app/controllers/home/Controller.php
@@ -4,25 +4,33 @@ namespace App\Controllers;
 
 include_once __DIR__ . "/../../core/controller/Controller.php";
 include_once __DIR__ . "/../../core/templates/DbTemplate.php";
+include_once __DIR__ . "/../../core/auth/Guard.php";
+include_once __DIR__ . "/../../core/auth/Session.php";
+
 include_once __DIR__ . "/../../models/holdings/Repository.php";
 
 use App\Core\Controller;
 use App\DbTemplate;
+use App\Core\Auth\AuthGuard;
+use App\Core\Session;
+
 use App\Models\Repository\HoldingRepository;
 
 class HomeController
 {
     private HoldingRepository $holdingRepository;
     private DbTemplate $db;
+    private Session $session;
 
     public function __construct() {
         $this->db = new DbTemplate();
         $this->holdingRepository = new HoldingRepository($this->db->getPdo());
+        $this->session = new Session();
     }
 
     public function index()
     { 
-        Controller::redirectIfNotAuth();
+        AuthGuard::redirectIfNotAuth($this->session);
         
         $holdings = $this->holdingRepository->findAll();
         
@@ -33,7 +41,7 @@ class HomeController
 
     public function favicon()
     {
-        Controller::redirectIfNotAuth();
+        AuthGuard::redirectIfNotAuth($this->session);
         Controller::render('favicon');
     }
 
