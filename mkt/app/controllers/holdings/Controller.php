c9c05e69adaeb05c997add097320b94bd03f27e8
Hemit Patel <hemitvpatel@gmail.com>
2025-08-29 12:29:44 -0400

    migrated holdings controller to authguard core service

--- mkt/app/controllers/holdings/Controller.php
+++ mkt/app/controllers/holdings/Controller.php
@@ -5,12 +5,18 @@ namespace App\Controllers;
 include_once __DIR__ . "/../../core/controller/Controller.php";
 include_once __DIR__ . "/../../core/templates/DbTemplate.php";
 include_once __DIR__ . "/../../core/files/Files.php";
+include_once __DIR__ . "/../../core/auth/Guard.php";
+include_once __DIR__ . "/../../core/auth/Session.php";
+
 include_once __DIR__ . "/../../models/holdings/Repository.php";
 include_once __DIR__ . "/../../models/user/Repository.php";
 
 use App\Core\Controller;
 use App\Core\Files;
 use App\DbTemplate;
+use App\Core\Session;
+use App\Core\Auth\AuthGuard;
+
 use App\Models\Repository\HoldingRepository;
 use App\Models\Repository\UserRepository;
 use Exception;
@@ -20,19 +26,21 @@ class HoldingsController
     private HoldingRepository $holdingRepository;
     private UserRepository $userRepository;
     private DbTemplate $db;
+    private Session $session;
 
     public function __construct() {
         $this->db = new DbTemplate();
         $this->holdingRepository = new HoldingRepository($this->db->getPdo());
         $this->userRepository = new UserRepository($this->db->getPdo());
+        $this->session = new Session();
     }
 
     public function sell()
     {
+        AuthGuard::redirectIfNotAuth($this->session);
         $this->db->getPdo()->beginTransaction();
 
         try {
-            $session = Controller::redirectIfNotAuth(returnSession: true);
             $id = filter_input(INPUT_POST, "id", FILTER_SANITIZE_NUMBER_INT);
 
             $holding = $this->holdingRepository->findById($id);
@@ -41,11 +49,11 @@ class HoldingsController
                 Controller::redirectToResult("Holding order needs to be fulfilled before selling", "error");
             }
 
-            $user = $this->userRepository->findByEmail($session['email']);
+            $user = $this->userRepository->findByEmail($this->session->getSession()['email']);
             $newBalance = $user['balance'] - $user['bought_price'];
 
-            $this->userRepository->updateBalance($newBalance, $session['email']);
-            $this->holdingRepository->delete($id, $session['email']);
+            $this->userRepository->updateBalance($newBalance, $this->session->getSession()['email']);
+            $this->holdingRepository->delete($id, $this->session->getSession()['email']);
             Files::deleteFile($holding['proposal_file']);
 
             $this->db->getPdo()->commit();
