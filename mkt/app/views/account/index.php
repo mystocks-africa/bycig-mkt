9ddb715204a591b05923ab4ef20cc1601daa5689
Hemit Patel <hemitvpatel@gmail.com>
2025-08-30 17:47:54 -0400

    add view

--- mkt/app/views/account/index.php
+++ mkt/app/views/account/index.php
@@ -6,6 +6,18 @@
     <title>Account</title>
 </head>
 <body>
-    Account
+    <div class="account">
+        <section class="account-user-info">
+            <h1><?= $user["full_name"] ?></h1>
+            <p class="small-gray-text"><?= $user["email"] ?></p>
+            <div class="flex-col-wrapper">
+                <a class="big-bold-black-text">Personal Info</a>
+                <a class="big-bold-black-text">Holdings</a>
+            </div>
+        </section>
+        <section class="account-content">
+            Select a section to view details
+        </section>
+    </div>
 </body>
 </html>
