f574c6ec68f981351899af46e3df8b7e72ecdd9d
Hemit Patel <hemitvpatel@gmail.com>
2025-08-30 12:57:39 -0400

    add conditional rendering where if fulfilled is true then we sell otherwise we can only buy

--- mkt/app/views/profile/index.php
+++ mkt/app/views/profile/index.php
@@ -86,15 +86,21 @@
                         <p><strong>Stock Ticker:</strong> <?= $stockTicker ?></p>
                         <p><strong>Investor:</strong> <?= $investor ?></p>
                         <p><strong>Fulfilled:</strong><?= $fulfilled ? 'yes' : 'no' ?></p>
-                        
-                        <form class="sell-btn-wrapper" action="/holdings/buy" method="POST">
-
-                            <input name="email" value="<?= $investor ?>" style="display:none;">
-                            <input name="id" value="<?= $id ?>" style="display:none;">
-
-                            <button class="sell-btn" type="submit">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-briefcase-business-icon lucide-briefcase-business"><path d="M12 12h.01"/><path d="M16 6V4a2 2 0 0 0-2-2h-4a2 2 0 0 0-2 2v2"/><path d="M22 13a18.15 18.15 0 0 1-20 0"/><rect width="20" height="14" x="2" y="6" rx="2"/></svg>                            
-                            </button>
+                        <? if ($fulfilled): ?>
+                                <form class="sell-btn-wrapper" action="/holdings/sell" method="POST">
+                                    <input name="email" value="<?= $investor ?>" style="display:none;">
+                                    <input name="id" value="<?= $id ?>" style="display:none;">
+                                    <form class="sell-btn-wrapper" action="/holdings/buy" method="POST">
+                                    <button class="sell-btn" type="submit">Sell</button>
+                                </form>
+                            <? else: ?>
+                                <form class="sell-btn-wrapper" action="/holdings/buy" method="POST">
+                                    <input name="email" value="<?= $investor ?>" style="display:none;">
+                                    <input name="id" value="<?= $id ?>" style="display:none;">
+                                    <form class="sell-btn-wrapper" action="/holdings/buy" method="POST">
+                                    <button class="sell-btn" type="submit">Buy</button>
+                                </form>
+                            <? endif; ?>
                         </form>
                     </div>
                 <?php endforeach; ?>
