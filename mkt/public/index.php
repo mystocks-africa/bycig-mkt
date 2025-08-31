f7b0e302e8f25fe4ef6f8f0720397f891aaa4c97
Hemit Patel <hemitvpatel@gmail.com>
2025-08-31 13:52:56 -0400

    remove navbar if on redirect

--- mkt/public/index.php
+++ mkt/public/index.php
@@ -1,6 +1,11 @@
 <?php
 require __DIR__ . '/../vendor/autoload.php';
 
-include __DIR__ . '/../app/views/navbar.php';
+$requestUri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+
+// Only include navbar if not on /redirect
+if ($requestUri !== '/redirect') {
+    include __DIR__ . '/../app/views/navbar.php';
+}
 
 $router = require __DIR__ . '/../app/core/routes/index.php';
