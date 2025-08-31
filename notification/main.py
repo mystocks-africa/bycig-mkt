598643ca66594d460cf9c62a14aa72be3b502d08
Hemit Patel <hemitvpatel@gmail.com>
2025-08-31 09:35:39 -0400

    remove python service (no longer needed)

--- notification/main.py
+++
@@ -1,9 +0,0 @@
-from classes.FinnhubWebSocket import FinnHubWebSocket
-
-class Main: 
-    def __init__(self):
-        finnHubWebSocket = FinnHubWebSocket()
-        finnHubWebSocket.start()
-
-if __name__ == "__main__":
-    Main()
