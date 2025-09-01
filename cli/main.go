5ee3196622a02cafb18f7c7c4a19f0643f386655
Hemit Patel <hemitvpatel@gmail.com>
2025-09-01 16:03:44 -0400

    add core basic cli

--- cli/main.go
+++ cli/main.go
@@ -3,5 +3,14 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello world!")
+	const email string 
+	const role string 
+
+	fmt.Println("Enter your email:")
+	fmt.Scan(&email)
+
+	fmt.Println("Enter your role:")
+	fmt.Scan(&role)
+
+	fmt.Println("Thank you!")
 }
