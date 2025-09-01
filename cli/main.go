e4cd3b8f57ec5961552a74232a20cb67d41bcd2d
Hemit Patel <hemitvpatel@gmail.com>
2025-09-01 16:09:26 -0400

    add if statement for role validity + change const to var + exit if invalid through os pkg

--- cli/main.go
+++ cli/main.go
@@ -1,16 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
-	const email string 
-	const role string 
+	var email string 
+	var role string 
 
 	fmt.Println("Enter your email:")
 	fmt.Scan(&email)
 
 	fmt.Println("Enter your role:")
 	fmt.Scan(&role)
+
+	if (role != "cluster_leader" && role != "user") {
+		fmt.Println("Invalid role. It is either 'cluster_leader' or 'user'")
+		os.Exit(0)
+	}
 
 	fmt.Println("Thank you!")
 }
