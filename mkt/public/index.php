2c9a67101f9ef0df0cb47ce7f79735b4ffdd1958
Hemit Patel <hemitvpatel@gmail.com>
2025-09-01 19:46:54 -0400

    add / to directory

--- mkt/public/index.php
+++ mkt/public/index.php
@@ -1,10 +1,10 @@
 <?php
-require $_SERVER['DOCUMENT_ROOT'] . '/vendor/autoload.php';
+require $_SERVER['DOCUMENT_ROOT'] . '/../vendor/autoload.php';
 
 $requestUri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
 if ($requestUri !== '/redirect') {
-    include $_SERVER['DOCUMENT_ROOT'] . '/app/views/navbar.php';
+    include $_SERVER['DOCUMENT_ROOT'] . '/../app/views/navbar.php';
 }
 
-$router = require $_SERVER['DOCUMENT_ROOT'] . '/app/core/routes/index.php';
+$router = require $_SERVER['DOCUMENT_ROOT'] . '/../app/core/routes/index.php';
