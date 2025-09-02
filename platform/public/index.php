979a3f748eed8b84516b0baf56de0034c8a3216b
Hemit Patel <hemitvpatel@gmail.com>
2025-09-01 22:01:56 -0400

    trying something else

--- platform/public/index.php
+++ platform/public/index.php
@@ -1,10 +1,9 @@
 <?php
-// Use $_SERVER['DOCUMENT_ROOT'] for absolute paths
 require __DIR__ . '/../vendor/autoload.php';
 
 $requestUri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 if ($requestUri !== '/redirect') {
-    include $_SERVER['DOCUMENT_ROOT'] . '/app/views/navbar.php';
+    include __DIR__ . '/app/views/navbar.php';
 }
 
 $router = require __DIR__ . "/../app/routes/index.php";
