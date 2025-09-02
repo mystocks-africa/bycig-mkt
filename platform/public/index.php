aba5695e66ede588f1f365d38ebb13ad5b8b146e
Hemit Patel <hemitvpatel@gmail.com>
2025-09-01 21:49:31 -0400

    trying a fix??

--- platform/public/index.php
+++ platform/public/index.php
@@ -1,14 +1,15 @@
 <?php
-require __DIR__ . '/../vendor/autoload.php';
+// Use $_SERVER['DOCUMENT_ROOT'] for absolute paths
+require $_SERVER['DOCUMENT_ROOT'] . '/vendor/autoload.php';
+
 
 if (!file_exists($autoloadPath)) {
     exit("Autoload file not found at: " . $autoloadPath);
 }
 
 $requestUri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-
 if ($requestUri !== '/redirect') {
-    include __DIR__ . '/../app/views/navbar.php';
+    include $_SERVER['DOCUMENT_ROOT'] . '/app/views/navbar.php';
 }
 
-$router = require __DIR__ . '/../app/routes/index.php';
+$router = require $_SERVER['DOCUMENT_ROOT'] . '/app/core/routes/index.php';
