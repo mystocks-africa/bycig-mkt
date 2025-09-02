ba161aa3cebca1ee126150396b4adac7279c3296
Hemit Patel <hemitvpatel@gmail.com>
2025-09-01 21:38:57 -0400

    remove the $_SERVER["DOCUMENT_ROOT"] and put __DIR__ instead

--- platform/public/index.php
+++ platform/public/index.php
@@ -4,7 +4,7 @@ require __DIR__ . '/../vendor/autoload.php';
 $requestUri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
 if ($requestUri !== '/redirect') {
-    include $_SERVER['DOCUMENT_ROOT'] . '/../app/views/navbar.php';
+    include __DIR__ . '/../app/views/navbar.php';
 }
 
-$router = require $_SERVER['DOCUMENT_ROOT'] . '/../app/core/routes/index.php';
+$router = require __DIR__ . '/../app/core/routes/index.php';
