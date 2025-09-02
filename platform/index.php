8a097eabe8ab9ea5006296c5843c38cd97e86145
Hemit Patel <hemitvpatel@gmail.com>
2025-09-02 13:24:38 -0400

    enable error handling

--- platform/index.php
+++ platform/index.php
@@ -1,4 +1,8 @@
 <?php
+// Enable error reporting
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
 require __DIR__ . '/vendor/autoload.php';
 
 $requestUri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
